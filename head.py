6463242f0655e9910d97c830192e60defd11d5a9
danishdeepsphere <120715128+danishdeepsphere@users.noreply.github.com>
2022-12-19 15:27:55 +0530

    Delete head.py

--- head.py
+++
@@ -1,5 +0,0 @@
-import streamlit as vAR_st
-def title():
-    vAR_st.markdown(" <h1 style='text-align: center; color: Black;font-size: 25px;'>Applied AI for schools-8th grade</h1>", unsafe_allow_html=True)
-    vAR_st.markdown("<p style='text-align: center; color: blue; font-size:29px;'>Simple application built on Streamlit</p>", unsafe_allow_html=True)
-    
