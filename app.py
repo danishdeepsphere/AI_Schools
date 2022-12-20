3b66e3785a05df49f6430dc243299bdff82fe908
danishdeepsphere <120715128+danishdeepsphere@users.noreply.github.com>
2022-12-20 14:18:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,21 +16,21 @@ with open('style/style2.css') as vAR_f:
 if 'placeholder' not in vAR_st.session_state:
     vAR_st.session_state.placeholder = "select"
 
-col1, col2, col3 = vAR_st.columns((3,5,3))
-with col1:
-    vAR_st.write("")
-with col2:
-    #vAR_img = img.open("image/Logo_final.png")
-    vAR_st.image('https://raw.githubusercontent.com/tarun243/Streamlit-commonToAllIndustry/master/Web_app/Logo_final.png',width=400)
-with col3:
-    vAR_st.write("")
+
 
 with vAR_st.sidebar:
     selected=vAR_st.selectbox("Menu",('Area & Perimeter','Factorial','Square and Cube','Statistics','Palindrome','Prime or not'),key="main")
     vAR_lib=vAR_st.selectbox("",("Libraries","Streamlit","Pillow"))
     vAR_st.button("Clear/Reset", on_click=cr.button_rad)
         
-
+col1, col2, col3 = vAR_st.columns((3,5,3))
+with col1:
+    vAR_st.write("")
+with col2:
+    #vAR_img = img.open("image/Logo_final.png")
+    vAR_st.image('https://raw.githubusercontent.com/tarun243/Streamlit-commonToAllIndustry/master/Web_app/Logo_final.png')
+with col3:
+    vAR_st.write("")
 if __name__=="__main__":
     try:
         if selected=="Area & Perimeter":
